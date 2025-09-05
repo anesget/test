8cade951b2521645717f2655a4dbe594e2156888
anes <anesboucenna234@gmail.com>
2025-09-05 17:49:20 +0100

    File Created

---
+++ index.js
@@ -0,0 +1,23 @@
+const { response } = require("express")
+
+let inp=document.getElementById('inp')
+let btn=document.getElementById('btn')
+let check=document.getElementById('check')
+let dv=document.getElementById('dv')
+const euro=270
+//events
+//loclastorage VS sessionStorage
+window.onload=async()=>{
+  try{
+  const response=await fetch("https://randomuser.me/api/")
+  if(response.ok){
+    const data=await response.json()
+    console.log(data.result[0].name.last)
+  }
+}
+catch(error){
+console.log(error)
+}
+}
+
+//gotlak 3afsa ma3rafe machi lahdia tathagag wla la
