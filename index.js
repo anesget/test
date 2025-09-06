b7863a937645c72acbc0fe0c6bd62932b7fd2f55
anes <anesboucenna234@gmail.com>
2025-09-06 11:07:20 +0100

    5

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 
-
-let inp=document.getElementById('inp')
-let btn=document.getElementById('btn')
-let check=document.getElementById('check')
-let dv=document.getElementById('dv')
 const euro=270
 let v=10
 //events
 //loclastorage VS sessionStorage
-window.onload=async()=>{
+const fun=async()=>{
   try{
   const response=await fetch("https://randomuser.me/api/")
   if(response.ok){
     const data=await response.json()
-    console.log(data.result[0].name.last)
+    console.log(data.results[0].name.last)
   }
 }
 catch(error){
 
 }
 }
-
+fun()
 //gotlak 3afsa ma3rafe machi lahdia tathagag wla la
